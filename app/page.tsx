878a613dce054a603e605747fcea1565b99b6bda
Amartya Sinha <amartua2002@gmail.com>
2023-08-07 13:53:07 +0530

    isuue: background image

--- app/page.tsx
+++ app/page.tsx
@@ -4,39 +4,37 @@ import { HomepageHero } from "@/components/sections/homepage-hero"
 
 export default function Home() {
   return (
-   
-    <div className="">
-  {/* First section */}
-  {/* <div className=" bg-[url('../public/test.png')] bg-cover w-full h-[100vh] "> </div> */}
-  
-  <div className="   w-full h-[100vh] bg-contain bg-repeat-round  top-0 left-0  bg-[url('../public/test.png')] ">
-  
-    <Container>
-    <div className="pt-[6.4rem]">
-      <HomepageHero />
-    </div>
-    </Container>
-    
-  </div>4
-  <div className="w-full h-[250vh]"></div>
-  
 
-  
+    <div className="  " >
+      
 
-  {/* Second section */}
-  {/* <div className="xl:top-[39rem] 2xl:top-[55rem] rounded-3xl shadow-xl w-[90vw] h-[100vh] top-[51rem] sm:top-[37rem] bg-white mx-auto relative">
+      <div className="   w-full h-[100vh]   ">
+
+        <Container>
+          <div className="pt-[6.4rem]">
+            <HomepageHero />
+          </div>
+
+        </Container>
+
+      </div>
+
+
+
+
+      <div className="xl:top-[39rem] 2xl:top-[55rem] rounded-3xl shadow-xl w-[90vw] h-[100vh] top-[51rem] sm:top-[37rem] bg-red-200 mx-auto relative">
     
   </div>
 
-  <div className="xl:top-[39rem] 2xl:top-[55rem] rounded-3xl shadow-xl  mt-20 w-[90vw] h-[100vh] top-[50rem] sm:top-[37rem] bg-white mx-auto relative">
+  <div className="xl:top-[39rem] 2xl:top-[55rem] rounded-3xl shadow-xl  mt-20 w-[90vw] h-[100vh] top-[50rem] sm:top-[37rem] bg-red-200 mx-auto relative">
     
-  </div> */}
+  </div> 
 
 
 
-</div>
+    </div>
 
-      
+
 
   )
 }
--- components/sections/homepage-hero.tsx
+++ components/sections/homepage-hero.tsx
@@ -5,7 +5,7 @@ export const HomepageHero = () => (
     
         <Hero>
 
-    <HeroTitle className="">
+    <HeroTitle className="font-bold">
     The easy way to build 
         <br className="hidden md:block" /> <span className="bg-gradient-to-r from-orange-500  to-yellow-500 bg-clip-text text-transparent">beautiful </span> websites
     </HeroTitle>
@@ -25,12 +25,14 @@ export const HomepageHero = () => (
 </button> */}
  
     
- <button className='relative inline-flex h-12 overflow-hidden rounded-full p-[2px] w-full sm:w-auto hover:scale-105 duration-300 active:scale-110'>
+ <button className='relative inline-flex h-12 overflow-hidden rounded-full p-[2px] w-full sm:w-auto hover:scale-105 duration-300 active:scale-110 mb-20'>
   <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
   <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-10 py-1  font-medium text-white backdrop-blur-3xl'>
     Explore More &#10132;
   </span>
 </button>
+
+<img src="heroimg1.png" alt="Hero Image" className=""/>
 
 </Hero>
     
